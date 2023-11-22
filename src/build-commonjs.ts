033fa9a25c98e410029b61744ee5db5ea183d795
Tim Suchanek <tim.suchanek@gmail.com>
2023-11-21 17:20:14 -0800

    only unlink/rename polyfills if file exists
    Fix: #35
    
    PR-URL: https://github.com/isaacs/tshy/pull/36
    Credit: @timsuchanek
    Close: #36
    Reviewed-by: @isaacs

--- src/build-commonjs.ts
+++ src/build-commonjs.ts
@@ -1,6 +1,6 @@
 import chalk from 'chalk'
 import { spawnSync } from 'node:child_process'
-import { renameSync, unlinkSync } from 'node:fs'
+import { existsSync, renameSync, unlinkSync } from 'node:fs'
 import { relative, resolve } from 'node:path/posix'
 import buildFail from './build-fail.js'
 import config from './config.js'
@@ -9,6 +9,10 @@ import polyfills from './polyfills.js'
 import setFolderDialect from './set-folder-dialect.js'
 import './tsconfig.js'
 const { commonjsDialects = [] } = config
+
+const unlinkIfExist = (f: string) => existsSync(f) && unlinkSync(f)
+const renameIfExist = (f: string, to: string) =>
+  existsSync(f) && renameSync(f, to)
 
 export const buildCommonJS = () => {
   setFolderDialect('src', 'commonjs')
@@ -33,10 +37,12 @@ export const buildCommonJS = () => {
         `.tshy-build/${d}`,
         relative(resolve('src'), resolve(orig))
       ).replace(/\.tsx?$/, '')
-      unlinkSync(`${stemTo}.js.map`)
-      unlinkSync(`${stemTo}.d.ts.map`)
-      renameSync(`${stemFrom}.cjs`, `${stemTo}.js`)
-      renameSync(`${stemFrom}.d.cts`, `${stemTo}.d.ts`)
+      const stemToPath = `${stemTo}.js.map`
+      const stemToDtsPath = `${stemTo}.d.ts.map`
+      unlinkIfExist(stemToPath)
+      unlinkIfExist(stemToDtsPath)
+      renameIfExist(`${stemFrom}.cjs`, `${stemTo}.js`)
+      renameIfExist(`${stemFrom}.d.cts`, `${stemTo}.d.ts`)
     }
     console.error(chalk.cyan.bold('built commonjs'))
   }
